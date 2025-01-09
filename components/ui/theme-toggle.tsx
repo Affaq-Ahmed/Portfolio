f0d14895536b57f0202f37066c248779db03473e
Affaq-Ahmed <affaqahmed165@gmail.com>
2025-01-09 20:35:00 +0500

    feat: Implement theme provider and toggle component for dark/light mode

---
+++ components/ui/theme-toggle.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+
+import { Button } from './button';
+import { MoonIcon, SunIcon } from 'lucide-react';
+
+const ThemeToggle = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button
+      size='sm'
+      variant={'ghost'}
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+    >
+      {resolvedTheme === 'dark' ? (
+        <SunIcon className='size-4 text-orange-300' />
+      ) : (
+        <MoonIcon className='size-4 text-sky-950' />
+      )}
+
+      <span className='sr-only'>Toggle theme</span>
+    </Button>
+  );
+};
+
+export default ThemeToggle;
