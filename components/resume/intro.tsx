bb2afd191fcebd94ae827c1c9b917e6dd87ed9a1
Affaq-Ahmed <affaqahmed165@gmail.com>
2025-01-21 18:51:14 +0500

    feat: add profile picture and update styles in ResumeIntro component

--- components/resume/intro.tsx
+++ components/resume/intro.tsx
@@ -1,6 +1,5 @@
 import { FaEnvelope, FaLocationPin } from 'react-icons/fa6';
 import React from 'react';
-import Image from 'next/image';
 
 export default function ResumeIntro() {
   return (
@@ -8,34 +7,29 @@ export default function ResumeIntro() {
       <h1 className='title-name no-underline'>Affaq Ahmed</h1>
 
       <div className='flex w-full flex-col gap-y-4'>
-        <h5 className='text-sm sm:text-base font-semibold'>
+        <h5 className='text-sm font-semibold sm:text-base'>
           Full Stack Web Developer & Designer
         </h5>
 
         <div className='flex w-full justify-between'>
           <div className='flex flex-col gap-1'>
-            <p className='flex items-center gap-2 sm:gap-4 text-xs sm:text-sm'>
+            <p className='flex items-center gap-2 text-xs sm:gap-4 sm:text-sm'>
               <FaLocationPin size={16} className='' /> From Sargodha, Pakistan
             </p>
-            <p className='flex items-center gap-2 sm:gap-4 text-sm'>
+            <p className='flex items-center gap-2 text-sm sm:gap-4'>
               <FaEnvelope size={16} />{' '}
               <a
                 href='mailto:affaqahmed165@gmail.com'
-                className='text-xs sm:text-sm font-medium underline hover:text-muted-foreground'
+                className='text-xs font-medium underline hover:text-muted-foreground sm:text-sm'
               >
                 affaqahmed165@gmail.com
               </a>
             </p>
           </div>
 
-          <Image
-            src='/images/authors/Affaq.jpeg'
-            alt={`Affaq Ahmed's profile picture`}
-            width={100}
-            height={100}
-            className='-mt-10 h-24 w-24 rounded-full border-4 sm:border-8 border-white grayscale hover:border-green-400 sm:-mt-20 sm:h-36 sm:w-36'
-            priority
-          />
+          <div
+            className={`-mt-20 h-[100px] w-[100px] bg-[url('/images/resume/Affaq_Ahmed_DP_1w.png')] bg-cover bg-no-repeat hover:bg-[url('/images/resume/Affaq_Ahmed_DP_2w.png')] dark:bg-[url('/images/resume/Affaq_Ahmed_DP_1b.png')] dark:hover:bg-[url('/images/resume/Affaq_Ahmed_DP_2b.png')] sm:-mt-2.5 sm:h-[150px] sm:w-[150px]`}
+          ></div>
         </div>
       </div>
     </section>
